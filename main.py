878d90ae945ebff396de6f0293d082491280ba19
Anthi Matthaiopoulou <121954653+anthimat11@users.noreply.github.com>
2023-05-07 17:57:34 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+#substract numbers contained in a list 
+def subtraction(numbers):
+    
+    for i in range(len(numbers)):
+        if( numbers[i].isnumeric()): numbers[i] = int(numbers[i])
+        else: numbers[i] = 0
+    
+    print("List of numbers: ", numbers)
+
+    total=numbers[0]
+    for i in range(1,len(numbers)):
+        total = total - numbers[i]
+    if (total>0):
+        print("The result is positive.")
+        return "POSITIVE"
+    elif (total<0):
+        print("The result is negative.")
+        return "NEGATIVE"
+    else: 
+        print("The result is 0.")
+        return "ZERO"
+
+
+
+##main program
+def main():
+    while True:
+        x = list(input("Enter multiple values: ").split())
+        if (x == ["q"] or x==["Q"] or x==[]): break
+        subtraction(x)
+        ##print(addition(x))
+
+
+##main()
